a0a12dacfcbd2abfc0ae94962512cbc6101d64a7
Hans Baier <hansfbaier@gmail.com>
2020-11-30 07:16:42 +0700

    dividing-counter: Add reset input

--- adat/sources/receiver/dividingcounter.py
+++ adat/sources/receiver/dividingcounter.py
@@ -6,6 +6,7 @@ from nmigen.back import verilog, rtlil
 
 class DividingCounter(Elaboratable):
     def __init__(self, divisor, width):
+        self.rst_in = Signal()
         self.clk_in = Signal()
         self.active_in = Signal()
         self.counter_out = Signal(width)
@@ -18,30 +19,36 @@ class DividingCounter(Elaboratable):
 
         dividing_cycle_counter = Signal(range(0, self.divisor))
 
-        with m.If(self.active_in):
-            with m.If(dividing_cycle_counter == self.divisor - 1):
-                m.d.sync += [
-                    dividing_cycle_counter.eq(0),
-                    self.divided_counter_out.eq(self.divided_counter_out + 1),
-                    self.dividable_out.eq(1)
-                ]
-
-            with m.Else():
-                m.d.sync += [
-                    self.dividable_out.eq(0),
-                    dividing_cycle_counter.eq(dividing_cycle_counter + 1)
-                ]
-
-            # when the main counter wraps around to zero, the dividing counter needs to reset too
-            with m.If(self.counter_out == (2 ** self.counter_out.width) - 1):
-                m.d.sync += dividing_cycle_counter.eq(0)
-
+        with m.If(self.rst_in):
             m.d.sync += [
-                self.counter_out.eq(self.counter_out + 1),
+                self.counter_out.eq(0),
+                self.divided_counter_out.eq(0),
+                dividing_cycle_counter.eq(0)
             ]
+        with m.Else():
+            with m.If(self.active_in):
+                with m.If(dividing_cycle_counter == self.divisor - 1):
+                    m.d.sync += [
+                        dividing_cycle_counter.eq(0),
+                        self.divided_counter_out.eq(self.divided_counter_out + 1),
+                        self.dividable_out.eq(1)
+                    ]
+
+                with m.Else():
+                    m.d.sync += [
+                        self.dividable_out.eq(0),
+                        dividing_cycle_counter.eq(dividing_cycle_counter + 1)
+                    ]
+
+                # when the main counter wraps around to zero, the dividing counter needs to reset too
+                with m.If(self.counter_out == (2 ** self.counter_out.width) - 1):
+                    m.d.sync += dividing_cycle_counter.eq(0)
+
+                m.d.sync += [
+                    self.counter_out.eq(self.counter_out + 1),
+                ]
 
         return m
-print(__name__)
 
 if __name__ == "__main__":
     dut = DividingCounter(5, 5)
@@ -49,12 +56,25 @@ if __name__ == "__main__":
     sim = Simulator(dut)
 
     def sync_process():
+        yield dut.active_in.eq(0)
         for _ in range(0, 5):
-            yield dut.active_in.eq(0)
             yield Tick()
 
+        yield dut.active_in.eq(1)
         for _ in range(0, 50):
-            yield dut.active_in.eq(1)
+            yield Tick()
+
+        yield dut.rst_in.eq(1)
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield dut.rst_in.eq(0)
+
+        for _ in range(0, 20):
             yield Tick()
 
         yield dut.active_in.eq(0)
