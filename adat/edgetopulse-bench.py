7e0361034ff065436ec4c1f328d3e4fe656e60a8
Hans Baier <hansfbaier@gmail.com>
2020-12-24 10:35:01 +0700

    edgetopulse: new module, sync (1 clock latency)

---
+++ adat/edgetopulse-bench.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from nmigen.sim import Simulator, Tick
+from edgetopulse import EdgeToPulse
+
+if __name__ == "__main__":
+    dut = EdgeToPulse()
+    sim = Simulator(dut)
+
+    def sync_process():
+        yield dut.edge_in.eq(0)
+        yield Tick()
+        yield dut.edge_in.eq(0)
+        yield Tick()
+        yield dut.edge_in.eq(0)
+        yield Tick()
+        yield dut.edge_in.eq(1)
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield dut.edge_in.eq(0)
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield dut.edge_in.eq(1)
+        yield Tick()
+        yield Tick()
+        yield dut.edge_in.eq(0)
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield dut.edge_in.eq(1)
+        yield Tick()
+        yield dut.edge_in.eq(0)
+        yield Tick()
+        yield dut.edge_in.eq(1)
+        yield dut.rst_in.eq(1)
+        yield Tick()
+        yield Tick()
+        yield Tick()
+        yield dut.rst_in.eq(0)
+        yield Tick()
+        yield Tick()
+        yield Tick()
+
+    sim.add_sync_process(sync_process)
+    sim.add_clock(1e-6)
+    with sim.write_vcd('edgetopulse.vcd', traces=[dut.edge_in, dut.pulse_out]):
+        sim.run()
---
+++ adat/edgetopulse.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""converts a rising edge to a single clock pulse"""
+from nmigen import Elaboratable, Signal, Module
+
+class EdgeToPulse(Elaboratable):
+    """
+        each rising edge of the signal edge_in will be
+        converted to a single clock pulse on pulse_out
+    """
+    def __init__(self):
+        self.rst_in           = Signal()
+        self.edge_in          = Signal()
+        self.pulse_out        = Signal()
+
+    def elaborate(self, platform) -> Module:
+        m = Module()
+
+        edge_shift = Signal(2)
+
+        with m.If(self.rst_in):
+            m.d.sync += edge_shift.eq(0)
+            m.d.comb += self.pulse_out.eq(0)
+
+        with m.Else():
+            m.d.sync += edge_shift.eq((edge_shift << 1) | self.edge_in)
+
+            with m.Switch(edge_shift):
+                with m.Case("01"):
+                    m.d.comb += self.pulse_out.eq(1)
+                with m.Default():
+                    m.d.comb += self.pulse_out.eq(0)
+
+        return m
