647108d19d74604ae8c1534d5fe69815fb4ce4c6
Hans Baier <hansfbaier@gmail.com>
2021-01-23 15:00:59 +0700

    nrzidecoder: clean up syntactic nmigen noise

--- adat/nrzidecoder.py
+++ adat/nrzidecoder.py
@@ -36,14 +36,16 @@ class NRZIDecoder(Elaboratable):
         """assemble the module"""
         m = Module()
         self.setup_clockdomains(m)
+        comb = m.d.comb
+        sync = m.d.sync
 
         nrzi      = Signal()
         nrzi_prev = Signal()
         got_edge  = Signal()
 
         m.submodules.cdc = FFSynchronizer(self.nrzi_in, nrzi)
-        m.d.sync += nrzi_prev.eq(nrzi)
-        m.d.comb += got_edge.eq(nrzi_prev ^ nrzi)
+        sync += nrzi_prev.eq(nrzi)
+        comb += got_edge.eq(nrzi_prev ^ nrzi)
 
         # we are looking for 10 non changing bits
         # and those will be ~900ns long @48kHz
@@ -56,21 +58,22 @@ class NRZIDecoder(Elaboratable):
 
         with m.FSM():
             with m.State("SYNC"):
-                m.d.comb += self.running.eq(0)
-                m.d.sync += [
+                comb += self.running.eq(0)
+                sync += [
                     self.data_out.eq(0),
                     self.data_out_en.eq(0)
                 ]
                 self.find_bit_timings(m, sync_counter, got_edge)
 
             with m.State("DECODE"):
-                m.d.comb += self.running.eq(1)
+                comb += self.running.eq(1)
                 self.decode_nrzi(m, bit_time, got_edge)
 
         return m
 
     def find_bit_timings(self, m: Module, sync_counter: DividingCounter, got_edge: Signal):
         """Waits for the ten zero bits of the SYNC section to determine the length of an ADAT bit"""
+        sync = m.d.sync
         bit_time_44100 = math.ceil(110 * (self.clk_freq/self.adat_freq(44100) / 100))
 
         # as long as the input does not change, count up
@@ -79,36 +82,37 @@ class NRZIDecoder(Elaboratable):
             # if the sync counter is 10% over the sync time @44100Hz, then
             # the signal just woke up from the dead. Start counting again.
             with m.If(sync_counter.counter_out > 10 * bit_time_44100):
-                m.d.sync += sync_counter.reset_in.eq(1)
+                sync += sync_counter.reset_in.eq(1)
 
             # if we are in the middle of the signal,
             # and got an edge, then we reset the counter on each edge
             with m.Else():
                 # when the counter is bigger than 3/4 of the old max, then we have a sync frame
                 with m.If(sync_counter.counter_out > 7 * bit_time_44100):
-                    m.d.sync += sync_counter.active_in.eq(0) # stop counting, we found it
+                    sync += sync_counter.active_in.eq(0) # stop counting, we found it
                     m.next = "DECODE"
                 with m.Else():
-                    m.d.sync += sync_counter.reset_in.eq(1)
+                    sync += sync_counter.reset_in.eq(1)
 
         # when we have no edge, count...
         with m.Else():
-            m.d.sync += [
+            sync += [
                 sync_counter.reset_in.eq(0),
                 sync_counter.active_in.eq(1)
             ]
 
     def decode_nrzi(self, m: Module, bit_time: Signal, got_edge: Signal):
         """Do the actual decoding of the NRZI bitstream"""
+        sync = m.d.sync
         bit_counter  = Signal(7)
         # this counter is used to detect a dead signal
         # to determine when to go back to SYNC state
         dead_counter = Signal(8)
         output       = Signal(reset=1)
 
-        m.d.sync += bit_counter.eq(bit_counter + 1)
+        sync += bit_counter.eq(bit_counter + 1)
         with m.If(got_edge):
-            m.d.sync += [
+            sync += [
                 # latch 1 until we read it in the middle of the bit
                 output.eq(1),
                 # resynchronize at each bit edge, 1 to compensate
@@ -118,27 +122,27 @@ class NRZIDecoder(Elaboratable):
                 dead_counter.eq(0)
             ]
         with m.Else():
-            m.d.sync += dead_counter.eq(dead_counter + 1)
+            sync += dead_counter.eq(dead_counter + 1)
 
         # wrap the counter
         with m.If(bit_counter == bit_time):
-            m.d.sync += bit_counter.eq(0)
+            sync += bit_counter.eq(0)
         # output at the middle of the bit
         with m.Elif(bit_counter == (bit_time >> 1)):
-            m.d.sync += [
+            sync += [
                 self.data_out.eq(output),
                 self.data_out_en.eq(1), # pulse out_en
                 output.eq(0) # edge has been output, wait for new edge
             ]
         with m.Else():
-            m.d.sync += self.data_out_en.eq(0)
+            sync += self.data_out_en.eq(0)
 
         # when we had no edge for 16 bits worth of time
         # then we go back to sync state
         with m.If(dead_counter >= bit_time << 4):
-            m.d.sync += dead_counter.eq(0)
+            sync += dead_counter.eq(0)
             m.next = "SYNC"
 
 if __name__ == "__main__":
-    module = NRZIDecoder()
+    module = NRZIDecoder(100e6)
     main(module, name="nrzi_decoder", ports=[module.nrzi_in, module.data_out])
